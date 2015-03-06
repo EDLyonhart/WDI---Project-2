bf46c0ee33c4118c52769d246d0a77bc5c3e3471
George Navas <gannavas@gmail.com>
2015-03-06 13:21:37 -0800

    updated users.rb to include logic for FB oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth, provider) 
+    where(provider: auth[:provider], uid: auth[:uid], name: auth[:info][:nickname]).first || create_from_omniauth(auth,provider)
+  end
+    
+  def self.create_from_omniauth(auth,provider)
+
+  create(provider: auth["provider"], uid: auth["uid"], name: auth["info"]["first_name"])
+  end
 end
