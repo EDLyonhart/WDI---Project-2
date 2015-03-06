30474f16be1374814445ea918a59c99b3663495c
George Navas <gannavas@gmail.com>
2015-03-06 14:25:02 -0800

    basic FB oauth completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth, provider) 
-    where(provider: auth[:provider], uid: auth[:uid], name: auth[:info][:nickname]).first || create_from_omniauth(auth,provider)
+    where(uid: auth[:uid], name: auth[:info][:nickname]).first || create_from_omniauth(auth,provider)
   end
     
   def self.create_from_omniauth(auth,provider)
 
-  create(provider: auth["provider"], uid: auth["uid"], name: auth["info"]["first_name"])
+  create(uid: auth["uid"],nickname: auth["info"]["nickname"],email: auth["info"]["email"],  
+    name: auth["info"]["name"],first_name: auth["info"]["first_name"],last_name: auth["info"]["last_name"],
+    profile_pic: auth["info"]["image"], location: auth["info"]["location"], gender: auth["extra"]["gender"]  )
   end
 end
