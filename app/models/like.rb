85d2a88f901ca50f69a3391f45ec8d40e78ff611
EDLyonhart <EDLyonhart@gmail.com>
2015-03-06 18:31:43 -0800

    typo

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ActiveRecord::Base
 belongs_to :user
-validates_presence_of :liker, :likee, :mutual
+validates_presence_of :liker, :likee, :is_matched
 validates :is_matched, :inclusion => {:in => [true, false]}
 
 end
