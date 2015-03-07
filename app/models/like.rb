679b8166f0855e33af38fce4df4b949d9e0751fb
EDLyonhart <EDLyonhart@gmail.com>
2015-03-06 19:14:46 -0800

    resolved presence/boolean conflict

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ActiveRecord::Base
 belongs_to :user
-validates_presence_of :liker, :likee, :is_matched
+validates_presence_of :liker, :likee
 validates :is_matched, :inclusion => {:in => [true, false]}
 
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,5 +2,4 @@ class Resource < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :user_id, :catagory, :presence
-  validates :presence, :inclusion => {:in => [true, false]}
 end
