f3bad97ba43fee5739ce32edaeb17a629fa46d68
George Navas <gannavas@gmail.com>
2015-03-07 13:45:34 -0800

    removed location validation

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,3 @@
 class MatchesController < ApplicationController
+  #This controller will be in charge of sending email messages to notify users of matches. 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ActiveRecord::Base
   has_many :likes
   has_many :reviews, as: :reviewable 
 
-  validates_presence_of :uid, :email, :name, :location
+  validates_presence_of :uid, :email, :name
 
   def self.from_omniauth(auth, provider)
     find_by(uid: auth[:uid]) || create_from_omniauth(auth,provider)
   end
 
   def self.create_from_omniauth(auth,provider)
-  create(uid: auth["uid"],nickname: auth["info"]["nickname"],email: auth["info"]["email"],
+  new(uid: auth["uid"],nickname: auth["info"]["nickname"],email: auth["info"]["email"],
     name: auth["info"]["name"],first_name: auth["info"]["first_name"],last_name: auth["info"]["last_name"],
     profile_pic: auth["info"]["image"], location: auth["info"]["location"], gender: auth["extra"]["gender"]  )
   end
