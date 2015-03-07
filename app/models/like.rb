79c8e86a8a3c6d97de5d0b43ee6f2e80c3b02c70
rycwilson <rycwilson@gmail.com>
2015-03-06 19:18:47 -0800

    pre-pull commit

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ActiveRecord::Base
 belongs_to :user
-validates_presence_of :liker, :likee, :is_matched
-validates :is_matched, :inclusion => {:in => [true, false]}
+validates_presence_of :liker, :likee
+
+validates :is_matched, :inclusion => { :in => [true, false] }
 
 end
