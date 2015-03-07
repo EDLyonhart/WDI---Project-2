38bc98d74cc18c8436bff2f924b4efa4cd8c1720
George Navas <gannavas@gmail.com>
2015-03-06 19:18:58 -0800

    boolean validators fix

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,4 @@
 class Like < ActiveRecord::Base
 belongs_to :user
 validates_presence_of :liker, :likee, :is_matched
-validates :is_matched, :inclusion => {:in => [true, false]}
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307020238) do
+ActiveRecord::Schema.define(version: 20150307021917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150307020238) do
   create_table "friends", force: :cascade do |t|
     t.integer  "uid"
     t.boolean  "is_user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "liker"
+    t.integer  "likee"
+    t.boolean  "is_matched"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
