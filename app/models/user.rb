49a13133fd4f28095be50f0a766fbd69d2382f00
rycwilson <rycwilson@gmail.com>
2015-03-06 17:54:26 -0800

    user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
-  def self.from_omniauth(auth, provider) 
+  has_many :users_friends
+  has_many :friends, through: :users_friends
+
+  def self.from_omniauth(auth, provider)
     find_by(uid: auth[:uid]) || create_from_omniauth(auth,provider)
   end
-    
+
   def self.create_from_omniauth(auth,provider)
-  create(uid: auth["uid"],nickname: auth["info"]["nickname"],email: auth["info"]["email"],  
+  create(uid: auth["uid"],nickname: auth["info"]["nickname"],email: auth["info"]["email"],
     name: auth["info"]["name"],first_name: auth["info"]["first_name"],last_name: auth["info"]["last_name"],
     profile_pic: auth["info"]["image"], location: auth["info"]["location"], gender: auth["extra"]["gender"]  )
   end
