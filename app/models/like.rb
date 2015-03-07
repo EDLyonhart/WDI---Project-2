27e02a6dcd670d9065408d93b4ea31aa7d7a1879
rycwilson <rycwilson@gmail.com>
2015-03-06 19:31:48 -0800

    Like table now working

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ActiveRecord::Base
 belongs_to :user
 
-validates_presence_of :liker, :likee
+validates_presence_of :user_id, :likee
 
 validates :is_matched, :inclusion => { :in => [true, false] }
 
