11c19a7c2fd957b7315d956ccf6d7c7d902bacb1
George Navas <gannavas@gmail.com>
2015-03-08 12:07:14 -0700

    Completed Likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+
+def like_check
+  #checks to see if there is a match
+@likee = User.find(params[:likee_id])
+@likee_likes = @likee.likes 
+  if @likee_likes.exists(likee: params[:id])
+    #if there is now a match - create new row in likes with boolean true; change other row's boolean to true as well
+      @like = Likes.new(user_id:params[:id],likee:params[:likee_id],is_matched:true)
+      Like.find_by(user_id:params[:likee_id]).update_attribute(is_matched:true)
+      #redirect to profile page - New Like
+      redirect_to user_home_path (params[:id])
+  else 
+      #if there is no match yet - add row to likes table and redirect back into the carousel 
+      @like = Likes.new(user_id:params[:id],likee:params[:likee_id],is_matched:false)
+      #Redirect back to carousel page
+      redirect_to matches_path (params[:id])
+  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
 #Users Routes
 root 'users#login'
 get '/login', to: 'users#login', as: "login"
-get '/user/:id', to: 'users#index', as: "user_home" #Home_page - Where user initially provieds has and wants info
+get '/user/:id/home', to: 'users#index', as: "user_home" #Home_page - Where user initially provieds has and wants info
 get '/user/:id/edit', to: 'users#edit', as: "user_edit" #Allows a user to edit profile page
-get '/user/:id/show', to: 'users#show', as: "user_show" #Allows other users to see profile pages of matches
-get '/user/:id/matches', to: 'users#matches', as: "matches" #Carousel - where swiping will occur (id used to populate queue)
-get '/user/:id/snippet', to: 'users#snippet', as: "snippet" #gets all Users and generates relevancy score to queue up carousel
+get '/user/:id', to: 'users#show', as: "user_show" #Allows other users to see profile pages of matches
+get '/user/:id/matches', to: 'users#matches', as: "matches" #Carousel - where swiping will occur (id used to populate queue)/gets all Users and generates relevancy score to queue up carousel
 get '/auth/:provider/callback', to: 'users#create', as: "create" #fb oauth
 get '/auth/:provider/callback', to: 'users#create_fb', as: "create_fb" #fb oauth
 put '/profile/:id/edit', to: 'users#update', as: "update"
@@ -21,7 +20,7 @@ get '/profile/:profile_id/resource/:id', to: 'resources#show', as: "user_resourc
 delete '/profile/:profile_id/resource/:id', to: 'resources#destroy'
 
 #Likes Routes
-post '/profile/:id/snippet/:likee_id', to: 'likes#check', as: "like_check"
+post '/user/:id/home/:likee_id', to: 'likes#check', as: "like_check"
 
 
 end
