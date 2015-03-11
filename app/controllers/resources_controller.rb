db9ab926d0f7848cfe52cebddebe9f38a38b60d6
George Navas <gannavas@gmail.com>
2015-03-10 21:22:18 -0700

    ability to add and delte resources confirmed

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -19,15 +19,19 @@ class ResourcesController < ApplicationController
     has_category = "has_#{resource_params[:category]}"
     wants_category = "wants_#{resource_params[:category]}"
     @resource = Resource.new resource_params
+    binding.pry
     # don't allow user to save identical resources
     unless resource_exists @user, @resource
       if @resource.save
-        update_user_table @user, @resource, "add"
+        @user.resources << @resource
+        # update_user_table @user, @resource, "add"
         # below will update category booleans in users table
-        if resource_params[:has] == true
-        @user.update_attribute(has_category.sym, true)
+        if resource_params[:has] == "1"
+          binding.pry
+        @user.update_attribute(has_category.to_sym, true)
         else 
-        @user.update_attribute(wants_category.sym, true)
+          binding.pry
+        @user.update_attribute(wants_category.to_sym, true)
         end
         #end of updating category booleans in users table 
         redirect_to user_home_path @user
@@ -45,11 +49,22 @@ class ResourcesController < ApplicationController
     resource = Resource.find params[:id] # No need for instance var here
     # is this check even going to be necessary?
     # when will one user be able to directly examine another user's resources?
+    has_category = "has_#{resource.category}"
+    wants_category = "wants_#{resource.category}"
     if session[:user_id] != resource.user_id
       flash[:alert] = "You are not authorized to delete this itinerary"
       redirect_to user_home_path
     else
-      update_user_table @user, resource, "remove"
+      # below will update category booleans in users table
+       if resource.has
+          binding.pry
+        @user.update_attribute(has_category.to_sym, false)
+        else 
+          binding.pry
+        @user.update_attribute(wants_category.to_sym, false)
+       end
+        #end of updating category booleans in users table 
+      # update_user_table @user, resource, "remove"
       resource.destroy
       redirect_to user_home_path
     end
@@ -71,29 +86,30 @@ class ResourcesController < ApplicationController
     end
   end
 
-  def update_user_table user, resource, add_or_remove
-    # add the resource to user.has_tags[] or user.want_tags[] as appropriate
-    @user = user
-    if add_or_remove == "add"
-      if resource.has
-        @user.has_tags << resource.category
-        @user.save
-      else
-        @user.want_tags << resource.category
-        @user.save
-      end
-    elsif add_or_remove == "remove"
-      if resource.has
-        @user.has_tags.delete resource.category
-        @user.save
-      else
-        @user.want_tags.delete resource.category
-        @user.save
-      end
-    else
-      flash[:alert] = "Something bad happened when updating user table"
-    end
-  end
+#commenting out def update_user_table - as this should no longer be necessary and should be replaced with boolean logic
+  # def update_user_table user, resource, add_or_remove
+  #   # add the resource to user.has_tags[] or user.want_tags[] as appropriate
+  #   @user = user
+  #   if add_or_remove == "add"
+  #     if resource.has
+  #       @user.has_tags << resource.category
+  #       @user.save
+  #     else
+  #       @user.want_tags << resource.category
+  #       @user.save
+  #     end
+  #   elsif add_or_remove == "remove"
+  #     if resource.has
+  #       @user.has_tags.delete resource.category
+  #       @user.save
+  #     else
+  #       @user.want_tags.delete resource.category
+  #       @user.save
+  #     end
+  #   else
+  #     flash[:alert] = "Something bad happened when updating user table"
+  #   end
+  # end
 
   def find_user
     @user = User.find session[:user_id]
