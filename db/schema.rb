e59714c31b270a7c3cbae73de6093c2b461e4273
Jenni Choi <jenni0729@gmail.com>
2015-03-12 11:42:57 -0700

    don't push

--- db/schema.rb
+++ db/schema.rb
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(version: 20150312013252) do
     t.integer  "reviewable_id"
     t.string   "reviewable_type"
     t.string   "content"
-    t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
